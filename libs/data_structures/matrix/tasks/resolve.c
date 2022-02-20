a1326b9ace79be907e4e493fb5b6bb6fed17e0c7
ElitePotatoes <elitepotatoman@gmail.com>
2022-02-20 16:16:44 +0300

    ninth

--- libs/data_structures/matrix/tasks/resolve.c
+++ libs/data_structures/matrix/tasks/resolve.c
@@ -1,3 +1,30 @@
 #include "resolve.h"
 
 /********************************************************* 9 **********************************************************/
+
+double getDistanceArray(const int a[], const size_t size) {
+    double distance = 0;
+    for (size_t i = 0; i < size; ++i)
+        distance += pow(a[i], 2);
+
+    return sqrt(distance);
+}
+
+void insertionSortRowsMatrixByRowCriteriaF(matrix m, float (*criteria)(int *, int)) {
+    float *criteriaArray = (float *) malloc(sizeof(float) * m.nRows);
+    for (size_t i = 0; i < m.nRows; ++i)
+        criteriaArray[i] = criteria(m.values[i], m.nCols);
+
+    for (size_t i = 0; i < m.nRows - 1; ++i)
+        for (size_t j = i + 1; j < m.nRows; ++j)
+            if (criteriaArray[i] > criteriaArray[j]) {
+                swapRows(m, i, j);
+                swapVoid(&criteriaArray[i], &criteriaArray[j], sizeof(float));
+            }
+
+    free(criteriaArray);
+}
+
+void sortByDistance(matrix m) {
+    insertionSortRowsMatrixByRowCriteriaF(m, getDistanceArray);
+}
--- libs/data_structures/matrix/tasks/resolve.h
+++ libs/data_structures/matrix/tasks/resolve.h
@@ -3,4 +3,10 @@
 
 #include "../matrix.h"
 
+//возвращает расстаояние до начала координат массива a размера size.
+double getDistanceArray(const int a[], size_t size);
+
+//сортирует строчки матрицы m по расстоянию от начала координат.
+void sortByDistance(matrix m);
+
 #endif //MAIN_C_RESOLVE_H
